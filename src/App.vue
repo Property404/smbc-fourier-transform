c3fe6189fa56557cd1c513808d54191eec04797b
Property404 <martinez.dagan@gmail.com>
2020-11-27 11:28:45 -0600

    Make fouriest app

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,61 @@
 <template>
-<input v-model="input_number">
-<div>bla</div>
-<button>Calculate</button>
+<h1>Fouriest Transform</h1>
+<form>
+<input v-on:changeValue="updateValue" v-model="input_number" autofocus>
+<div v-if="!warning">{{output_number}}<sub>10</sub> ⇒ {{this.output_number_based}}<sub>{{this.output_base}}</sub></div>
+<div v-if="warning">{{warning}}</div>
+<button v-on:click.prevent="updateValue" type="submit">Calculate</button>
+</form>
 </template>
 
 <script>
+import {presentAsBase, fouriest} from "./fourier";
 export default {
   name: 'App',
+  data(){
+    return{
+      input_number:624,
+      output_number:null,
+      output_number_based:null,
+      output_base:null,
+      warning:null
+    }
+  },
   components: {
+  },
+  beforeMount(){
+    this.updateValue();
+  },
+  methods:{
+    updateValue()
+    {
+      this.output_number = parseInt(this.input_number,10);
+      this.warning = null;
+      if(this.output_number<1)
+      {
+        this.warning = "Input must be greater than one";
+      }
+      else if(this.output_number != this.input_number)
+      {
+        this.warning = "Input should be integer";
+      }
+      else if(this.output_number>800000)
+      {
+        this.warning = "Number too large";
+      }
+      else
+      {
+        this.output_base = fouriest(this.input_number)
+        if(this.output_base === 0)
+          this.output_base = 10;
+        this.output_number_based = presentAsBase(this.output_number, this.output_base);
+      }
+    }
   }
 }
 </script>
 
-<style>
+<style lang="scss">
 html,body{
   margin:0;
   padding:0;
@@ -22,15 +65,39 @@ html,body{
 #app {
   display:flex;
   flex-direction:column;
-  justify-content:space-between;
-  align-items:space-between;
   height:100%;
   padding:0;
+  font-size:2rem;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin:0;
+  h1{
+    font-size:2.5rem;
+    padding:.2rem;
+    margin:0;
+  }
+  form{
+    height:100%;
+    display:flex;
+    flex-direction:column;
+    justify-content:space-between;
+    input,button{
+        min-height:3rem;
+        font-size:inherit;
+        text-align:center;
+    }
+    button{
+      background-color:blue;
+      border:none;
+      color:white;
+      cursor:pointer;
+      &:hover{
+        background-color:#0000cc;
+      }
+    }
+  }
 }
 </style>
--- src/fourier.js
+++ src/fourier.js
@@ -1,10 +1,10 @@
-function fouriest(number)
+export function fouriest(number)
 {
 	let base_with_most_fours = 0;
 	let highest_amount_of_fours = 0;
 	for(let base=2;base<(number>4?number:6);base++)
 	{
-		const max = Math.floor(Math.log(number, base));
+		const max = Math.floor(Math.log(number)/Math.log(base));
 		let value = number;
 		let fours = 0;
 		for(let power=max;power>=0;power--)
@@ -24,5 +24,32 @@ function fouriest(number)
 	}
 	return base_with_most_fours;
 }
+function convertToDigit(number)
+{
+	if(number<10)
+		return number;
+	else
+		return `{${number}}`
+}
+export function presentAsBase(value, base)
+{
+	let result = "";
+	const max = Math.floor(Math.log(value)/Math.log(base));
+	for(let power=max;power>=0;power--)
+	{
+		const base2power = base**power;
+		const times = Math.floor(value/base2power);
+		value-=base2power*times;
+		if(times || result!=="")
+		{
+			result+=convertToDigit(times);
+		}
+	}
+	return result;
+}
+console.assert(presentAsBase(16,2)==="10000");
+console.assert(presentAsBase(16,10)==="16");
+console.assert(presentAsBase(16,16)==="10");
+console.assert(presentAsBase(5,2)==="101");
 console.assert(fouriest(10) === 6);
 console.assert(fouriest(624) === 5);
